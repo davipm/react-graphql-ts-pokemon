c50573f31b856aa45e4603144fa2ef04f42e5f93
davi <davi.p.m94@gmail.com>
2020-08-17 00:23:52 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 
-import Global from "./styles/Global";
+import Global from "./styles/global";
 import Pokemons from "./components/Pokemons";
 
 function App() {
   useEffect(() => {
-    AOS.init({
-      once: true,
-    });
+    AOS.init({ once: true });
   }, []);
 
   return (
-    <div className="App">
+    <div id="app">
       <Pokemons />
       <Global />
     </div>
