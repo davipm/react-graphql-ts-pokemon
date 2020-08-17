61ee43923d38878e382223046f7bc0f5c7c0930e
davi <davi.p.m94@gmail.com>
2020-08-17 00:23:17 -0300

    update types

--- src/__types__/pokemonsTypes.ts
+++ src/__types__/pokemonsTypes.ts
@@ -1,18 +1,14 @@
-interface ISpecialName {
-  name: string;
-  type: string
-}
-
-interface ISpecial {
-  special: ISpecialName[]
-}
-
 export interface IPokemon {
   id: string;
   name: string;
-  number: string
-  image: string
-  maxHP: number
-  maxCP: number
-  attacks: ISpecial
+  number: string;
+  image: string;
+  maxHP: number;
+  maxCP: number;
+  attacks: {
+    special: {
+      name: string;
+      type: string;
+    }[];
+  };
 }
